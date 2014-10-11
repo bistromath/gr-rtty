d14f2d477a93f3abedaaa7d3e263802ca162fdee
Chris Kuethe <chris.kuethe@gmail.com>
2014-10-11 12:38:48 -0700

    remove build directory

--- build/apps/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /backup/gnuradio/oot/gr-rtty/apps
-# Build directory: /backup/gnuradio/oot/gr-rtty/build/apps
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/swig/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /backup/gnuradio/oot/gr-rtty/swig
-# Build directory: /backup/gnuradio/oot/gr-rtty/build/swig
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
